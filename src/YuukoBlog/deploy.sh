bea0346f43fea99186915e15d09cc9b528932282
hance <ce.han@aihuihsou.com>
2018-03-27 17:25:15 +0800

    update

--- src/YuukoBlog/deploy.sh
+++ src/YuukoBlog/deploy.sh
@@ -1,4 +1,4 @@
 ﻿
 docker build -t yuukoblogbuild -f /root/project/yuukoblog/deploy/dockerfile.deploy /root/project/yuukoblog/deploy
 docker rm -f yuukoblog
-docker run -d --name yuukoblog -p 5000:5000  -v /root/docker-volumes/blog/sqlite:/dotnetapp/sqlite yuukoblogbuild
+docker run -d --name yuukoblog -p 5000:5000  -v /root/docker/volumes/blog/sqlite:/dotnetapp/sqlite yuukoblogbuild
